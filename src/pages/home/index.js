c7ed39262fa3ae1d069e60866c1ed98f32294939
Caine Daniel <develop@cainethings.com>
2025-05-28 07:09:41 +0530

    Fin

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -74,7 +74,7 @@ const Home = () => {
 
   const handleTestDownload = () => {
     const phoneNumber = localStorage.getItem('phoneNumber');
-    fetch('http://localhost:8888/getPayslip.php', {
+    fetch('https://api-stories.cainethings.com/getPayslip.php', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
