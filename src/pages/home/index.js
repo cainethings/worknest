ac086a4c5fa801777946dd21266f2526a0d24efc
Caine Daniel <develop@cainethings.com>
2025-05-29 21:37:24 +0530

    Reinstate

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,146 @@
 // src/Home.js
-import React from 'react';
-import Layout from '../../layouts/primary'
+import React, { useState, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Layout from '../../layouts/primary';
 import './home.scss';
 import pageContent from './content.json';
+import html2pdf from 'html2pdf.js';
+
+import HiddenData from './table.js';
+
+
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [selectedMonth, setSelectedMonth] = useState('');
+  const [payslipData, setPayslipData] = useState(null);
+  const payslipRef = useRef();
+
+  // Redirect if already logged in
+  useEffect(() => {
+    const token = localStorage.getItem('isLoggedIn');
+    if (!token) navigate('/');
+  }, [navigate]);
+
+  const handleLogout = () => {
+    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('phoneNumber');
+    localStorage.removeItem('otp');
+    window.location.href = '/';
+  };
+
+  const handleDownload = () => {
+    const phoneNumber = localStorage.getItem('phoneNumber');
+    fetch('https://api-worknest.cainethings.com/getPayslip.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        month: selectedMonth,
+        phone: phoneNumber,
+      }),
+    }).then(async (response) => {
+      const data = await response.json();
+      const paySlipDataMain = data.data;
+      setPayslipData(paySlipDataMain);
+      setTimeout(() => {
+        html2pdf().set({
+          margin: 1,
+          filename: `Payslip-April-2025.pdf`,
+          image: { type: 'jpeg', quality: 0.98 },
+          html2canvas: { scale: 2 },
+          jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+        }).from(payslipRef.current).save().then(() => {
+          alert('Download completed successfully!');
+        });
+      }, 100);
+    }).catch((error) => {
+      console.error('Fetch error:', error);
+    });
+  };
+
+  const getMonthsFromStart = () => {
+    const startMonth = 3; // April (0-indexed)
+    const startYear = 2025;
+
+    const now = new Date();
+    const months = [];
+
+    // Target is last completed month
+    let endMonth = now.getMonth() - 1;
+    let endYear = now.getFullYear();
+
+    if (endMonth < 0) {
+      endMonth = 11;
+      endYear -= 1;
+    }
+
+    let currentMonth = startMonth;
+    let currentYear = startYear;
+
+    while (
+      currentYear < endYear ||
+      (currentYear === endYear && currentMonth <= endMonth)
+    ) {
+      const date = new Date(currentYear, currentMonth, 1);
+      const month = date.toLocaleString('default', { month: 'long' });
+      const year = String(currentYear).slice(-2);
+      months.push(`${month}-${year}`);
+
+      // Increment month and year
+      currentMonth++;
+      if (currentMonth > 11) {
+        currentMonth = 0;
+        currentYear++;
+      }
+    }
+
+    return months;
+  };
+
   return (
     <Layout pageContent={pageContent}>
-      
+      <section className='hero'>
+        <h1 className='section-title'>Hello!</h1>
+        <p className='instruction'>Select the month you want to download your payslip for.</p>
+
+        <div className='dropdown-container'>
+          <select
+            className='month-dropdown'
+            value={selectedMonth}
+            onChange={(e) => setSelectedMonth(e.target.value)}
+          >
+            <option value=''>-- Select Month --</option>
+            {getMonthsFromStart().map((month) => (
+              <option key={month} value={month}>
+                {month}
+              </option>
+            ))}
+          </select>
+          {/* 
+          <button className='download-button' onClick={handleDownload}>
+            Download Payslip
+          </button> */}
+          <button className='download-button' onClick={handleDownload}>
+            Download PDF
+          </button>
+        </div>
+        <br /><br /><br /><br /><br />
+        <button onClick={handleLogout} className='logout-button'>
+          Logout
+        </button>
+
+        {/* Hidden Payslip HTML for PDF generation */}
+        {payslipData && (
+          <div style={{ display: 'none' }}>
+            <div ref={payslipRef} className='payslip-preview'>
+              <HiddenData payslipData={payslipData} selectedMonth={selectedMonth} />
+              <p style={{ marginTop: "30px" }}><b>Note:</b> This is system generated payslip and does not require signature</p>
+            </div>
+          </div>
+        )}
+      </section>
     </Layout>
   );
 };
---
+++ src/pages/home/table.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import banner from '../../assets/images/banner.png';
+const HiddenData = (props) => {
+    const formatCurrency = (amount) =>
+        `${Number(amount).toLocaleString('en-IN', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        })}`;
+    const excelSerialToDateString = (serialStr) => {
+        if (!serialStr) return "";
+
+        const serial = parseFloat(serialStr.replace(/,/g, ""));
+        if (isNaN(serial)) return serialStr;
+
+        const excelEpoch = new Date(1900, 0, 1);
+        const days = Math.floor(serial) - 2;
+        const date = new Date(excelEpoch.getTime() + days * 24 * 60 * 60 * 1000);
+
+        return date.toLocaleDateString("en-GB", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        }); // e.g. "14 February 2018"
+    }
+    const payslipData = props.payslipData;
+    const selectedMonth = props.selectedMonth;
+    return (
+        <table style={{ width: '100%', borderCollapse: 'collapse' }} border='1'>
+            <thead>
+                <tr className='banner-row'>
+                    <th colSpan="4" style={{ textAlign: 'center', padding: '0px' }}>
+                        <img
+                            src={banner}
+                            crossOrigin="anonymous"
+                            style={{ maxWidth: '100%', height: 'auto' }}
+                        />
+                    </th>
+                </tr>
+                <tr className='title-row'>
+                    <th colSpan={4} style={{ textAlign: 'center', padding: '0px' }}>FORM XI Rules 26(2)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style={{ padding: "5px" }} colSpan={3}>Pay Slip For The Month</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }}>{selectedMonth}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>NAME</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }} colSpan={2}>Mr. {payslipData["FULL NAME"]}</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }}>{payslipData["Designation"]}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>ID</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }}>{payslipData["ID"]}</td>
+                    <td style={{ padding: "5px" }}>Basic Rate</td>
+                    <td style={{ padding: "5px", fontWeight: "bold", textAlign: 'right' }}>{payslipData["Basic Rate"] === "" ? "0.00" : payslipData["Basic Rate"]}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>Days Paid</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }}>{Number(payslipData["Days Paid"]).toFixed(1)}</td>
+                    <td style={{ padding: "5px" }}>{payslipData["F/D"] === "D" ? "OT Hours" : ""}</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }}>{payslipData["F/D"] === "D" ? Number(payslipData["OT Hours"]).toFixed(1) : ""}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>D.O.J</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }} colSpan={3}>
+                        {excelSerialToDateString(payslipData["D.O.J"])}
+                    </td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>UAN</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }} colSpan={3}>{payslipData["UAN"]}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>Bank</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }} colSpan={3}>{payslipData["Bank"]}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>A/c No</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }} colSpan={3}>{payslipData["A/c No"]}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>IFSC</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }} colSpan={3}>{payslipData["IFSC"]}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px", fontWeight: "bold" }} colSpan={2}>EARNING</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }} colSpan={2}>DEDUCTION</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>Wages Earned</td>
+                    <td style={{ padding: "5px", textAlign: 'right' }}>{payslipData["Wages Earned"] === "" ? "0.00" : payslipData["Wages Earned"]}</td>
+                    <td style={{ padding: "5px" }}>EPF</td>
+                    <td style={{ padding: "5px", textAlign: 'right' }}>{payslipData["EPF"] === "" ? "0.00" : payslipData["EPF"]}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>{payslipData["F/D"] === "D" ? "OT Amount" : "HRA"}</td>
+                    <td style={{ padding: "5px", textAlign: 'right' }}>{payslipData["F/D"] === "D" ? payslipData["OT Amount"] === "" ? "0.00" : payslipData["OT Amount"] : payslipData["HRA"] === "" ? "0.00" : payslipData["HRA"]}</td>
+                    <td style={{ padding: "5px" }}>Mess</td>
+                    <td style={{ padding: "5px", textAlign: 'right' }}>{payslipData["Mess"] === "" ? "0.00" : payslipData["Mess"]}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>{payslipData["F/D"] === "D" ? "H Allowance" : "Proj Allowance"}</td>
+                    <td style={{ padding: "5px", textAlign: 'right' }}>{payslipData["F/D"] === "D" ? payslipData["H Allowance"] === "" ? "0.00" : payslipData["H Allowance"] : payslipData["Proj Allowance"] === "" ? "0.00" : payslipData["Proj Allowance"]}</td>
+                    <td style={{ padding: "5px" }}>Advance</td>
+                    <td style={{ padding: "5px", textAlign: 'right' }}>{payslipData["Advance"] === "" ? "0.00" : formatCurrency(parseFloat(payslipData["Advance"]).toFixed(2))}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>{payslipData["F/D"] === "D" ? "S Allowance" : "V Allowance"}</td>
+                    <td style={{ padding: "5px", textAlign: 'right' }}>{payslipData["F/D"] === "D" ? payslipData["S Allowance"] === "" ? "0.00" : payslipData["S Allowance"] : payslipData["V Allowance"] === "" ? "0.00" : payslipData["V Allowance"]}</td>
+                    <td style={{ padding: "5px" }}>TDS</td>
+                    <td style={{ padding: "5px", textAlign: 'right' }}>{payslipData["TDS"] === "" ? "0.00" : formatCurrency(parseFloat(payslipData["TDS"]).toFixed(2))}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>Bonus @ 8.33%</td>
+                    <td style={{ padding: "5px", textAlign: 'right' }}>{payslipData["Bonus @ 8.33%"] === "" ? "0.00" : payslipData["Bonus @ 8.33%"]}</td>
+                    <td style={{ padding: "5px" }}></td>
+                    <td style={{ padding: "5px" }}></td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>Area Allowance</td>
+                    <td style={{ padding: "5px", textAlign: 'right' }}>{payslipData["Area Allowance"] === "" ? "0.00" : payslipData["Area Allowance"]}</td>
+                    <td style={{ padding: "5px" }}></td>
+                    <td style={{ padding: "5px" }}></td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>Washing Allowance</td>
+                    <td style={{ padding: "5px", textAlign: 'right' }}>{payslipData["Washing Allowance"] === "" ? "0.00" : payslipData["Washing Allowance"]}</td>
+                    <td style={{ padding: "5px" }}></td>
+                    <td style={{ padding: "5px" }}></td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px" }}>Performance Allowance</td>
+                    <td style={{ padding: "5px", textAlign: 'right' }}>{payslipData["Performance Allowance"] === "" ? "0.00" : payslipData["Performance Allowance"]}</td>
+                    <td style={{ padding: "5px" }}></td>
+                    <td style={{ padding: "5px" }}></td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px", fontWeight: "bold" }}>GROSS AMT</td>
+                    <td style={{ padding: "5px", fontWeight: "bold", textAlign: 'right' }}>{payslipData["GROSS AMT"] === "" ? "0.00" : payslipData["GROSS AMT"]}</td>
+                    <td style={{ padding: "5px", fontWeight: "bold" }}>Total Deduction</td>
+                    <td style={{ padding: "5px", fontWeight: "bold", textAlign: 'right' }}>{payslipData["Total Deduction"] === "" ? "0.00" : payslipData["Total Deduction"]}</td>
+                </tr>
+                <tr>
+                    <td style={{ padding: "5px", fontWeight: "bold" }} colSpan="2">Net Pay Credited to Bank A/c</td>
+                    <td style={{ padding: "5px", fontWeight: "bold", textAlign: 'center' }} colSpan="2">{payslipData["Net Pay Credited to Bank A/c"] === "" ? "₹0.00" : "₹" + payslipData["Net Pay Credited to Bank A/c"]}</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default HiddenData;
